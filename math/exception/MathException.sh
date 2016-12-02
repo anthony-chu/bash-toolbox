612d0ba805063c3f01761bf499fe9841956b92fa
Anthony Chu <anthony.chu@liferay.com>
2016-12-02 12:18:31 -0800

    Quote offending argument

--- math/exception/MathException.sh
+++ math/exception/MathException.sh
@@ -2,7 +2,7 @@ include math.exception.util.MathExceptionUtil
 
 MathException(){
 	notANumberException(){
-		MathExceptionUtil logErrorMsg "${1}_is_not_a_number"
+		MathExceptionUtil logErrorMsg "\"${1}\"_is_not_a_number"
 		exit
 	}
 
