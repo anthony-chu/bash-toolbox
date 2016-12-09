0c928eb37aa219a491316d5bff51e35e02ef535a
Anthony Chu <anthony.chu@liferay.com>
2016-12-08 16:41:14 -0800

    Update variable scope

--- formatter/Formatter.sh
+++ formatter/Formatter.sh
@@ -12,28 +12,28 @@ include string.validator.StringValidator
 
 Formatter(){
 	applyUnixLineEndings(){
-		file=${1}
+		local file=${1}
 
 		FileIOUtil replace ${file} "\r\n" "\n"
 	}
 
 	convertSpacesToTabs(){
-		file=${1}
+		local file=${1}
 
 		sed -i "s/[ ][ ]/\t/g" ${file}
 		sed -i "s/[ ][ ][ ][ ]/\t/g" ${file}
 	}
 
 	enforceBashToolboxLocalVariables(){
-		file=${1}
+		local file=${1}
 
 		if [[ $(StringValidator isSubstring ${file} bash-toolbox) ]]; then
-			n=1
+			local n=1
 
 			while read line; do
 				if [[ ${line} =~ [a-zA-Z]+= && ${line} != export* ]]; then
 					if [[ ${line} != *for* && ${line} != local* ]]; then
-						f=${file}
+						local f=${file}
 
 						local _message=(set_variable_scope_to_local_ ${f}:${n})
 
@@ -47,56 +47,56 @@ Formatter(){
 					fi
 				fi
 
-				n=$(MathUtil increment ${n})
+				local n=$(MathUtil increment ${n})
 			done < ${file}
 		fi
 	}
 
 	enforceLoggerMessageQuotes(){
-		file=${1}
+		local file=${1}
 
-		lineNumber=1
+		local lineNumber=1
 
 		while read line; do
 			if [[ ${line} =~ Logger && ${line} != *Completed* ]]; then
 
 				if [[ ${line} != *\"* && ${line} == *log*Msg* ]]; then
-					n=${lineNumber}
+					local n=${lineNumber}
 
 					Logger logErrorMsg "unquoted_log_message:_${file}:${n}"
 				fi
 			fi
 
-			lineNumber=$(MathUtil increment ${lineNumber})
+			local lineNumber=$(MathUtil increment ${lineNumber})
 		done < ${file}
 	}
 
 	verifyCharacterLimitPerLine(){
-		file=${1}
+		local file=${1}
 
-		lineNumber=1
+		local lineNumber=1
 
 		if [[ ! $(BaseComparator isEqual ${file} build.sh) ]]; then
 			while read line; do
-				length=$(StringUtil length ${line})
+				local length=$(StringUtil length ${line})
 
 				if [[ ${length} -le 9 ]]; then
-					length=$(StringUtil append 0 ${length})
+					local length=$(StringUtil append 0 ${length})
 				fi
 
 				if [[ ${length} > 80 && ${line} != *gitpr* ]]; then
-					n=${lineNumber}
+					local n=${lineNumber}
 
 					Logger logErrorMsg "character_limit_exceeded:_${file}:${n}"
 				fi
 
-				lineNumber=$(MathUtil increment ${lineNumber})
+				local lineNumber=$(MathUtil increment ${lineNumber})
 			done < ${file}
 		fi
 	}
 
 	verifyNoIncludesInBase(){
-		file=${1}
+		local file=${1}
 
 		if [[ $(StringValidator isSubstring ${file} Base) && ! $(StringValidator
 			isSubstring ${file} Test) ]]; then
