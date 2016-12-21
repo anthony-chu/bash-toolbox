89d09361d805c2b0a043b502560ffa68a8128e0a
Anthony Chu <anthony.chu@liferay.com>
2016-12-21 14:55:05 -0800

    Move command parsing into validateCommand()

--- command/validator/CommandValidator.sh
+++ command/validator/CommandValidator.sh
@@ -2,10 +2,13 @@ include array.util.ArrayUtil
 include array.validator.ArrayValidator
 
 include command.exception.CommandException
+include command.parser.CommandParser
 
 CommandValidator(){
 	validateCommand(){
-		if [[ ! $(ArrayValidator hasEntry ${@}) ]]; then
+		local validCommands=($(CommandParser getValidFunctions ${1}))
+
+		if [[ ! $(ArrayValidator hasEntry ${validCommands[@]} ${2}) ]]; then
 			local _flip=($(ArrayUtil flipArray ${@}))
 			local cmd=${_flip[0]}
 
