970285f79f36aba12a19fd53310892b6d46539d6
Anthony Chu <anthony.chu@liferay.com>
2017-01-05 15:03:28 -0800

    Add LanguageUtil tests

---
+++ test/LanguageUtilTest.sh
@@ -0,0 +1,50 @@
+include language.util.LanguageUtil
+
+include test.executor.TestExecutor
+
+LanguageUtilTest(){
+	run(){
+		local tests=(
+			toggleArticleVowelForm[false]
+			toggleArticleVowelForm[true]
+			togglePlurality[false]
+			togglePlurality[true]
+		)
+
+		TestExecutor executeTest LoggerUtilTest ${tests[@]}
+	}
+
+	testToggleArticleVowelForm[false](){
+		if [[ $(LanguageUtil toggleArticleVowelForm cba) == a ]]; then
+			echo PASS
+		else
+			echo FAIL
+		fi
+	}
+
+	testToggleArticleVowelForm[true](){
+		if [[ $(LanguageUtil toggleArticleVowelForm abc) == an ]]; then
+			echo PASS
+		else
+			echo FAIL
+		fi
+	}
+
+	testTogglePlurality[false](){
+		if [[ $(LanguageUtil togglePlurality 1 bar bars) == bar ]]; then
+			echo PASS
+		else
+			echo FAIL
+		fi
+	}
+
+	testTogglePlurality[true](){
+		if [[ $(LanguageUtil togglePlurality 2 bar bars) == bars ]]; then
+			echo PASS
+		else
+			echo FAIL
+		fi
+	}
+
+	$@
+}
