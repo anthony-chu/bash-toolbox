3350102e997c9e81a63bce738b2167296d44b502
Anthony Chu <anthony.chu@liferay.com>
2017-01-10 14:21:57 -0800

    Determine the tests dynamically so that we don't need to pass in all the tests names

--- test/executor/TestExecutor.sh
+++ test/executor/TestExecutor.sh
@@ -1,19 +1,23 @@
+include command.validator.CommandValidator
+
 include string.util.StringUtil
 
 TestExecutor(){
 	executeTest(){
-		local group=${1}
-		shift
-		local tests=(${@})
+		local _tests=(
+			$(CommandValidator getValidFunctions bash-toolbox/test/${1}.sh)
+		)
 
-		for test in ${tests[@]}; do
-			local testName=$(StringUtil capitalize ${test})
+		local tests=()
 
-			if [[ $(${group} test${testName}) == FAIL ]]; then
-				echo ${group}#test${testName}
+		for _test in ${_tests[@]}; do
+			if [[ ${_test} != ${1} && ${_test} != run ]]; then
+				if [[ $(${1} ${_test}) == FAIL ]]; then
+					echo ${1}\#${_test}
+				fi
 			fi
 		done
 	}
 
-	${@}
+	$@
 }
