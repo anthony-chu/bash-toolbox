9eb2704501491bd93d551753d568bbd94a2a6b8b
Anthony Chu <anthony.chu@liferay.com>
2017-01-25 15:47:04 -0800

    Copy cleanSource() from build-tool into GitUtil

--- git/util/GitUtil.sh
+++ git/util/GitUtil.sh
@@ -1,4 +1,26 @@
+include base.comparator.BaseComparator
+
+include logger.Logger
+
+include math.util.MathUtil
+
 GitUtil(){
+	cleanSource(){
+		Logger logProgressMsg "resetting_the_source_directory"
+
+		cd ${buildDir}
+
+		git reset --hard -q
+
+		if [[ $(MathUtil isEven $(BaseUtil getDate -d)) || $(
+			BaseComparator isEqual ${1} true) ]] ; then
+
+			git clean -fdqx -e "*.${HOSTNAME}.properties"
+		fi
+
+		Logger logCompletedMsg
+	}
+
 	getCurBranch(){
 		git rev-parse --abbrev-ref HEAD
 	}
