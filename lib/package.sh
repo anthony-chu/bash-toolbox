f143b3204a64604b983de93787ef1abba5af7ef0
Anthony Chu <anthony.chu@liferay.com>
2017-02-09 16:42:06 -0800

    Add protection into package.sh to prevent empty package imports

--- lib/package.sh
+++ lib/package.sh
@@ -1,9 +1,14 @@
 package(){
-	local files=($(find bash-toolbox/${1//./\/} -type f -iname "*.sh"))
+	if [[ ${1} ]]; then
+		local files=($(find bash-toolbox/${1//./\/} -type f -iname "*.sh"))
 
-	for file in ${files[@]}; do
-		if [[ ${file} != *lib* ]]; then
-			source ${file}
-		fi
-	done
+		for file in ${files[@]}; do
+			if [[ ${file} != *lib* ]]; then
+				source ${file}
+			fi
+		done
+	else
+		echo -e "[ ERROR ] \033[0;31m"Cannot import an empty package."\033[0m"
+		exit
+	fi
 }
