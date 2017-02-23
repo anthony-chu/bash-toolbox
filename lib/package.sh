e4245fb6acd445191b5a02d3cc1c50ade32f32dd
Anthony Chu <anthony.chu@liferay.com>
2017-02-23 11:58:57 -0800

    Enforce module size minimum for using package

--- lib/package.sh
+++ lib/package.sh
@@ -2,6 +2,12 @@ package(){
 	if [[ ${1} ]]; then
 		local files=($(find bash-toolbox/${1//./\/} -type f -iname "*.sh"))
 
+		if [ ${#files[@]} -lt 3 ]; then
+			echo -e "[ ERROR ] \033[0;31m"The package \"${1}\" is not large enough to source using package."\033[0m"
+			echo -e "[ ERROR ] \033[0;31m"Please use \"include\" instead on files from ${1}."\033[0m"
+			exit
+		fi
+
 		for file in ${files[@]}; do
 			if [[ ${file} != *lib* ]]; then
 				source ${file}
