de8f81ade903339ec596a44831f237c018987d27
Anthony Chu <anthony.chu@liferay.com>
2017-03-08 09:19:12 -0800

    Extract date-related methods to CalendarUtil (no logic change)

--- base/util/BaseUtil.sh
+++ base/util/BaseUtil.sh
@@ -17,16 +17,6 @@ BaseUtil(){
 		fi
 	}
 
-	getDate(){
-		if [[ ${1} == -m ]]; then
-			date +%m
-		elif [[ ${1} == -y ]]; then
-			date +%Y
-		elif [[ ${1} == -d ]]; then
-			date +%d
-		fi
-	}
-
 	gitpr(){
 		source "d:/git-tools/git-pull-request/git-pull-request.sh"
 	}
@@ -46,20 +36,6 @@ BaseUtil(){
 			echo $(timestamp log) [ INFO_ ] Configuring Liferay to use JDK7...
 			export JAVA_HOME="C:\Program Files\Java\jdk1.7.0_80"
 			echo $(timestamp log) [ INFO_ ] Completed.
-		fi
-	}
-
-	timestamp(){
-		if [[ ${1} == clock ]]; then
-			local t=$(date +%T%s)
-			echo ${t//[:]/}
-		elif [[ ${1} == date ]]; then
-			date +%Y%m%d
-		elif [[ ${1} == log ]]; then
-			echo $(date +%Y-%m-%d) $(date +%H:%M:%S)
-		else
-			local ms=$(date +%S%N)
-			date +%H:%M:${ms:0:3}
 		fi
 	}
 
---
+++ calendar/util/CalendarUtil.sh
@@ -0,0 +1,29 @@
+include string.util.StringUtil
+
+CalendarUtil(){
+	getDate(){
+		if [[ ${1} == -m ]]; then
+			date +%m
+		elif [[ ${1} == -y ]]; then
+			date +%Y
+		elif [[ ${1} == -d ]]; then
+			date +%d
+		fi
+	}
+
+	getTimestamp(){
+		if [[ ${1} == clock ]]; then
+			local t=$(date +%T%s)
+			StringUtil strip t [:]
+		elif [[ ${1} == date ]]; then
+			date +Y%m%d
+		elif [[ ${1} == log ]]; then
+			echo $(date +%Y-%m-%d) $(date +%H:%M:%S)
+		else
+			local ms=$(date +%S%N)
+			date +%H:%M:${ms:0:3}
+		fi
+	}
+
+	$@
+}
