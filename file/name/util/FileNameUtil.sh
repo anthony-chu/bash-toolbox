d9be02acb85501ed3f143e7c06a01c378e68acd1
Anthony Chu <anthony.chu@liferay.com>
2017-03-15 11:59:06 -0700

    Update usage

--- file/name/util/FileNameUtil.sh
+++ file/name/util/FileNameUtil.sh
@@ -1,12 +1,12 @@
-include os.validator.OSValidator
-
 include string.util.StringUtil
+
+include system.validator.SystemValidator
 
 FileNameUtil(){
 	getPath(){
 		local path=${1}
 
-		if [[ $(OSValidator isWindows) ]]; then
+		if [[ $(SystemValidator isWindows) ]]; then
 			local drive=$(StringUtil capitalize ${path:1:1})
 			local headlessPath=${path/\/[a-z]/}
 
