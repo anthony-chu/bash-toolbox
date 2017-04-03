43a4f672f153cd7895d93af757b61e44dab24a04
Anthony Chu <anthony.chu@liferay.com>
2017-04-03 16:42:34 -0700

    Allow toggling of path format

--- file/name/util/FileNameUtil.sh
+++ file/name/util/FileNameUtil.sh
@@ -1,18 +1,42 @@
-include string.util.StringUtil
+include base.comparator.BaseComparator
 
-include system.validator.SystemValidator
+include string.util.StringUtil
+include string.validator.StringValidator
 
 FileNameUtil(){
-	getPath(){
+	_getPathUnix(){
 		local path=${1}
 
-		if [[ $(SystemValidator isWindows) ]]; then
+		if [[ $(StringValidator isSubstring ${path} :) ]]; then
+			echo /$(StringUtil replace path [A-Z]: $(
+				StringUtil toLowerCase ${path:0:1}))
+			return
+		fi
+
+		echo ${path}
+	}
+
+	_getPathWin(){
+		local path=${1}
+
+		if [[ ! $(StringValidator isSubstring ${path} :) ]]; then
 			local drive=$(StringUtil capitalize ${path:1:1})
 			local headlessPath=${path/\/[a-z]/}
 
 			echo ${drive}:${headlessPath}
-		else
-			echo ${path}
+			return
+		fi
+
+		echo ${path}
+	}
+
+	getPath(){
+		local path=${2}
+
+		if [[ $(BaseComparator isEqual ${1} win) ]]; then
+			_getPathWin ${2}
+		elif [[ $(BaseComparator isEqual ${1} nix) ]]; then
+			_getPathUnix ${2}
 		fi
 	}
 
