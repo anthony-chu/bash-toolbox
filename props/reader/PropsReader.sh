da7f51bd148f4d053332d5ab7cf13dac7234d2ab
Anthony Chu <anthony.chu@liferay.com>
2017-04-13 15:32:30 -0700

    Add classes to retrieve property values from *.properties files

---
+++ props/reader/PropsReader.sh
@@ -0,0 +1,29 @@
+include props.reader.util.PropsReaderUtil
+
+PropsReader(){
+	readAppServerProps(){
+		${readProps} ${_buildDir}/app.server.${HOSTNAME}.properties ${2}
+	}
+
+	readBuildProps(){
+		${readProps} ${_buildDir}/build.${HOSTNAME}.properties ${2}
+	}
+
+	readFakeProps(){
+		${readProps} ${_buildDir}/fake.${HOSTNAME}.properties ${2}
+	}
+
+	readPortalProps(){
+		${readProps} ${_bundleDir}/portal-ext.properties ${2}
+	}
+
+	readTestProps(){
+		${readProps} ${_buildDir}/test.${HOSTNAME}.properties ${2}
+	}
+
+	local _bundleDir=$(BaseVars returnBundleDir ${2})
+	local _buildDir=$(BaseVars returnBuildDir ${2})
+	local readProps="PropsReaderUtil readProps"
+
+	$@
+}
---
+++ props/reader/util/PropsReaderUtil.sh
@@ -0,0 +1,57 @@
+include array.util.ArrayUtil
+
+include logger.util.LoggerUtil
+
+include props.validator.PropsValidator
+
+include string.util.StringUtil
+
+PropsReaderUtil(){
+	getPropsFileName(){
+		local _propsFile=${1}
+		local propsFile=($(StringUtil split _propsFile /))
+
+		local _path=$(ArrayUtil trim propsFile 1)
+
+		echo $(StringUtil replace _path space /) ${propsFile[-1]}
+	}
+
+	getValue(){
+		local output=$(cat ${1} | grep ${2})
+
+		local value=($(StringUtil split output =))
+
+		echo ${value[-1]}
+	}
+
+	readProps(){
+		local propsFileMap=($(getPropsFileName ${1}))
+
+		if [[ ! -e ${1} ]]; then
+			local message=(
+				property_file_\"${propsFileMap[1]}\"_does_not_exist_in_
+				${propsFileMap[0]}
+			)
+
+			Logger logErrorMsg "$(StringUtil join message)"
+			return
+		fi
+
+		if [[ $(PropsValidator propertyExists ${1} ${2}) ]]; then
+			getValue ${1} ${2}
+		else
+			local message=(
+				property_\"${2}\"_does_not_exist_in_
+				${propsFileMap[1]}
+			)
+
+			Logger logErrorMsg $(StringUtil join message)
+			return
+		fi
+	}
+
+	local _bundleDir=$(BaseVars returnBundleDir ${2})
+	local _buildDir=$(BaseVars returnBuildDir ${2})
+
+	$@
+}
