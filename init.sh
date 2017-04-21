5d485192ab71cc8ae931d62e3d24d6d3d4ce8a4e
Anthony Chu <anthony.chu@liferay.com>
2017-04-21 14:44:42 -0700

    Add library function to add annotations

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 source ${projectDir}bash-toolbox/lib/colorme.sh
 source ${projectDir}bash-toolbox/lib/include.sh
+source ${projectDir}bash-toolbox/lib/markups.sh
 source ${projectDir}bash-toolbox/lib/package.sh
 source ${projectDir}bash-toolbox/lib/readvar.sh
