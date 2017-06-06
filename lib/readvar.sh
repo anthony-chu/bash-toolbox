bce9a58b3808164c80e2e42643ff2e29d9da45dd
Anthony Chu <anthony.chu@liferay.com>
2017-06-06 09:06:16 -0700

    Variables cannot begin with numerical values

--- lib/readvar.sh
+++ lib/readvar.sh
@@ -1,7 +1,7 @@
 readvar(){
 	if [[ ${1} != *[a-zA-Z0-9_]* || ${1} == *[-.]* ]]; then
 		echo "${@}"
-	elif [[ ${!1} ]]; then
+	elif [[ ${!1} && ${1} != [0-9]* ]]; then
 		local var=${1}[@]
 
 		echo ${!var}
