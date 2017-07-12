54292de168bae422609d87e06f820114408a03e1
Anthony Chu <anthony.chu@liferay.com>
2017-07-12 14:02:21 -0700

    Only include functions not marked as private or ignored

--- command/validator/CommandValidator.sh
+++ command/validator/CommandValidator.sh
@@ -6,14 +6,25 @@ include string.util.StringUtil
 
 CommandValidator(){
 	getValidFunctions(){
+		local lineNumber=1
 		local file=${1}
 
 		local validFunctions=()
 
 		while read line; do
-			if [[ ${line} == *\(\){ && ! ${line} =~ @ignore ]]; then
-				validFunctions+=($(StringUtil strip line \(\)\{))
+			if [[ ${line} == *\(\){ ]]; then
+				local prevLineNumber=$((${lineNumber}-1))
+
+				local prevLineContent=$(sed "${prevLineNumber}q;d" ${file})
+
+				if [[ ! ${prevLineContent} =~ @ignore &&
+					! ${prevLineContent} =~ @private ]]; then
+
+					validFunctions+=($(StringUtil strip line \(\)\{))
+				fi
 			fi
+
+			local lineNumber=$((${lineNumber}+1))
 		done < ${file}
 
 		echo ${validFunctions[@]}
