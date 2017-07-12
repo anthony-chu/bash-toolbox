5340da4e783cc03470aecd25a1f2693d52dbc9f7
Anthony Chu <anthony.chu@liferay.com>
2017-07-12 13:16:18 -0700

    Add function to assert file or directory exists

--- test/util/TestUtil.sh
+++ test/util/TestUtil.sh
@@ -24,6 +24,24 @@ TestUtil(){
 		fi
 	}
 
+	assertExists(){
+		local path=$(readvar ${1})
+
+		if [[ $(StringValidator isSubstring path ".") ]]; then
+			if [[ -e ${path} ]]; then
+				echo PASS
+			else
+				echo FAIL
+			fi
+		else
+			if [[ -d ${path} ]]; then
+				echo PASS
+			else
+				echo FAIL
+			fi
+		fi
+	}
+
 	assertNull(){
 		if [[ $(StringValidator isNull ${1}) ]]; then
 			echo PASS
