d4b9551b6ee3b3e194e716f6f838bc1dda4dfcf9
Anthony Chu <anthony.chu@liferay.com>
2017-07-12 10:09:45 -0700

    No need for newline

--- file/util/FileUtil.sh
+++ file/util/FileUtil.sh
@@ -44,8 +44,7 @@ FileUtil(){
 		local fileName=$(FileNameUtil getPath nix ${1})
 		local _fileNameArray=($(StringUtil split fileName /))
 		local fileNameArray=($(ArrayUtil trim _fileNameArray 1))
-		local filePath=$(
-			construct /$(StringUtil replace fileNameArray space /))
+		local filePath=$(construct /$(StringUtil replace fileNameArray space /))
 
 		for cmd in {touch,echo}; do
 			${cmd} ${filePath}/${_fileNameArray[-1]}
