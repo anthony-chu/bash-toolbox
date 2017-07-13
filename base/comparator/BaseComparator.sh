117e42c7074992e52f779c26415e039559c63f73
Anthony Chu <anthony.chu@liferay.com>
2017-07-13 16:54:56 -0700

    Comparison differs between numerical and non-numerical strings

--- base/comparator/BaseComparator.sh
+++ base/comparator/BaseComparator.sh
@@ -12,14 +12,26 @@ BaseComparator(){
 	}
 
 	isLessThan(){
-		if [[ ${1} < ${2} ]]; then
-			echo true
+		if [[ ! ${1//[0-9-]/} && ! ${2//[0-9-]/} ]]; then
+			if [[ ${1} -lt ${2} ]]; then
+				echo true
+			fi
+		else
+			if [[ ${1} < ${2} ]]; then
+				echo true
+			fi
 		fi
 	}
 
 	isGreaterThan(){
-		if [[ ${1} > ${2} ]]; then
-			echo true
+		if [[ ! ${!//[0-9-]/} && ! ${2//[0-9-]/} ]]; then
+			if [[ ${1} -gt ${2} ]]; then
+				echo true
+			fi
+		else
+			if [[ ${1} > ${2} ]]; then
+				echo true
+			fi
 		fi
 	}
 
