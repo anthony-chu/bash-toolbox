2888b8c314f39cb20da3e7f8ec330143b2737897
Anthony Chu <anthony.chu@liferay.com>
2017-07-14 09:32:32 -0700

    Use PropsWriterUtil module

--- props/writer/PropsWriter.sh
+++ props/writer/PropsWriter.sh
@@ -1,43 +1,47 @@
 include base.vars.BaseVars
 
+include props.writer.util
+
 PropsWriter(){
 	setAppServerProps(){
-		_setProps ${appServerProps} ${2} ${3}
+		${setProps} ${appServerProps} ${2} ${3}
 	}
 
 	setBuildProps(){
-		_setProps ${buildProps} ${2} ${3}
+		${setProps} ${buildProps} ${2} ${3}
 	}
 
 	setPortalProps(){
-		_setProps ${portalProps} ${2} ${3}
+		${setProps} ${portalProps} ${2} ${3}
 	}
 
 	setTestProps(){
-		_setProps ${testProps} ${2} ${3}
+		${setProps} ${testProps} ${2} ${3}
 	}
 
 	unsetAppServerProps(){
-		_disableProps ${appServerProps} ${2}
+		${disableProps} ${appServerProps} ${2}
 	}
 
 	unsetBuildProps(){
-		_disableProps ${buildProps} ${2}
+		${disableProps} ${buildProps} ${2}
 	}
 
 	unsetPortalProps(){
-		_disableProps ${portalProps} ${2}
+		${disableProps} ${portalProps} ${2}
 	}
 
 	unsetTestProps(){
-		_disableProps ${testProps} ${2}
+		${disableProps} ${testProps} ${2}
 	}
 
 	local _buildDir=$(BaseVars returnBuildDir ${2})
 	local _bundleDir=$(BaseVars returnBundleDir ${2})
 	local appServerProps=${_buildDir}/app.server.${HOSTNAME}.properties
 	local buildProps=${_buildDir}/build.${HOSTNAME}.proeprties
+	local disableProps="PropsWriterUtil disableProps"
 	local portalProps=${_bundleDir}/portal-ext.properties
+	local setProps="PropsWriterUtil setProps"
 	local testProps=${_buildDir}/test.${HOSTNAME}.properties
 
 	$@
