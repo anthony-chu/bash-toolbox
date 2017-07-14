bf448682feb4ca6a1fd7346dbb608bfeed6aca2d
Anthony Chu <anthony.chu@liferay.com>
2017-07-14 08:39:07 -0700

    SF

--- props/reader/PropsReader.sh
+++ props/reader/PropsReader.sh
@@ -4,23 +4,23 @@ include props.reader.util.PropsReaderUtil
 
 PropsReader(){
 	readAppServerProps(){
-		${readProps} ${_buildDir}/app.server.${HOSTNAME}.properties ${2}
+		${readProps} ${buildDir}/app.server.${HOSTNAME}.properties ${2}
 	}
 
 	readBuildProps(){
-		${readProps} ${_buildDir}/build.${HOSTNAME}.properties ${2}
+		${readProps} ${buildDir}/build.${HOSTNAME}.properties ${2}
 	}
 
 	readPortalProps(){
-		${readProps} ${_bundleDir}/portal-ext.properties ${2}
+		${readProps} ${bundleDir}/portal-ext.properties ${2}
 	}
 
 	readTestProps(){
-		${readProps} ${_buildDir}/test.${HOSTNAME}.properties ${2}
+		${readProps} ${buildDir}/test.${HOSTNAME}.properties ${2}
 	}
 
-	local _bundleDir=$(BaseVars returnBundleDir ${2})
-	local _buildDir=$(BaseVars returnBuildDir ${2})
+	local bundleDir=$(BaseVars returnBundleDir ${2})
+	local buildDir=$(BaseVars returnBuildDir ${2})
 	local readProps="PropsReaderUtil readProps"
 
 	$@
