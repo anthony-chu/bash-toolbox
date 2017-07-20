a7564aeeeda413abee7ade669629e2fdb45aaf97
Anthony Chu <anthony.chu@liferay.com>
2017-07-20 16:58:50 -0700

    Add class and functions to interact with log file

---
+++ log/file/LogFile.sh
@@ -0,0 +1,36 @@
+include app.server.validator.AppServerValidator
+
+include base.vars.BaseVars
+
+include calendar.util.CalendarUtil
+
+include git.util.GitUtil
+
+include file.util.FileUtil
+
+include string.util.StringUtil
+
+LogFile(){
+	get(){
+		local appServer=$(AppServerValidator returnAppServer ${1})
+		local branch=$(BaseVars returnBranch ${2})
+
+		local logFile=(/d/logs/${branch}/${appServer}/
+			$(CalendarUtil getTimestamp date)/
+			${branch}-build-$(GitUtil getSHA ${branch} short)-
+			$(CalendarUtil getTimestamp clock).log
+		)
+
+		FileUtil makeFile $(StringUtil join logFile)
+	}
+
+	write(){
+		local logFile=$(readvar ${1})
+		shift
+		local cmd=$(readvar ${2})}
+
+		${cmd} |& tee -a ${logFile}
+	}
+
+	$@
+}
