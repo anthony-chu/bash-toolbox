3b196f04e2d4af54bc4664bf14245676fe902796
Anthony Chu <anthony.chu@liferay.com>
2017-07-20 08:14:10 -0700

    Rename exception

--- git/exception/GitException.sh
+++ git/exception/GitException.sh
@@ -1,16 +1,16 @@
 include logger.Logger
 
 GitException(){
-	branchDoesNotExistException(){
-		Logger logErrorMsg "cannot_${1}_${2}_because_${2}_does_not_exist"
-	}
-
 	curBranchException(){
 		Logger logErrorMsg "cannot_${1}_${2}_because_${2}_is_the_current_branch"
 	}
 
 	existingBranchException(){
 		Logger logErrorMsg "cannot_${1}_${2}_because_${2}_already_exists"
+	}
+
+	noSuchBranchException(){
+		Logger logErrorMsg "cannot_${1}_${2}_because_${2}_does_not_exist"
 	}
 
 	$@
