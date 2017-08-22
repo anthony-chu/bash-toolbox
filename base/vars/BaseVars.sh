27a4c802dfbc7fdebc0d73cdc30aea326a3d94c7
Anthony Chu <anthony.chu@liferay.com>
2017-08-22 13:43:26 -0700

    Add private branches

--- base/vars/BaseVars.sh
+++ base/vars/BaseVars.sh
@@ -58,7 +58,9 @@ BaseVars(){
 			*ee-6.2.10*) echo ee-6.2.10;;
 			*6.1.x*) echo 6.1.x;;
 			*6.2.x*) echo 6.2.x;;
+			*master-private*) echo master-private;;
 			*master*) echo master;;
+			*7.0.x-private*) echo 7.0.x-private;;
 			*7.0.x*) echo 7.0.x;;
 			*) echo master;;
 		esac
