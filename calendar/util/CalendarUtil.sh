d1776bbbed67f6eff51f169cc7a561ba048db96f
Anthony Chu <anthony.chu@liferay.com>
2017-09-13 10:08:10 -0700

    Add method to return a previous day's timestamp

--- calendar/util/CalendarUtil.sh
+++ calendar/util/CalendarUtil.sh
@@ -1,3 +1,5 @@
+include math.util.MathUtil
+
 include string.util.StringUtil
 
 CalendarUtil(){
@@ -33,6 +35,60 @@ CalendarUtil(){
 
 			date +%H:%M:${ms:0:3}
 		fi
+	}
+
+	returnYesterdayTimestamp(){
+		if [[ ${1} ]]; then
+			local this_day=${1:6:2}
+			local this_month=${1:4:2}
+			local this_year=${1:0:4}
+		else
+			local this_day=$(getDate -d)
+			local this_month=$(getDate -m)
+			local this_year=$(getDate -y)
+		fi
+
+		local prev_day=${this_day}
+		local prev_month=${this_month}
+		local prev_year=${this_year}
+
+		if [[ ${this_day} == 01 ]]; then
+			if [[ "01 02 04 06 08 09 11" =~ ${this_month} ]]; then
+				local prev_day=31
+			elif [[ ${this_month} == 03 ]]; then
+				if [[ $(MathUtil isDivisible ${this_year} 4) ]]; then
+					local prev_day=29
+				else
+					local prev_day=28
+				fi
+			else
+				local prev_day=30
+			fi
+
+			if [[ ${this_month} == 01 ]]; then
+				local prev_month=12
+				local prev_year=$(MathUtil decrement ${this_year})
+			elif [[ ${this_month} == 0* ]]; then
+				local prev_month=$(
+					MathUtil decrement $(StringUtil strip this_month 0))
+			else
+				local prev_month=$(MathUtil decrement ${this_month})
+			fi
+
+			if [[ ${#prev_month} == 1 ]]; then
+				local prev_month=0${prev_month}
+			fi
+		elif [[ ${this_day} == 0* ]]; then
+			local prev_day=$(MathUtil decrement $(StringUtil strip this_day 0))
+		else
+			local prev_day=$(MathUtil decrement ${this_day})
+		fi
+
+		if [[ ${#prev_day} == 1 ]]; then
+			local prev_day=0${prev_day}
+		fi
+
+		echo ${prev_year}${prev_month}${prev_day}
 	}
 
 	$@
