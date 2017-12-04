3f4f82f3d40f14ee605c30da682c9e0acb633373
Anthony Chu <anthony.chu@liferay.com>
2017-12-04 14:51:05 -0800

    New LoggerUtil tests

--- logger/util/test/LoggerUtilTest.sh
+++ logger/util/test/LoggerUtilTest.sh
@@ -8,12 +8,20 @@ LoggerUtilTest(){
 		TestExecutor executeTest LoggerUtilTest
 	}
 
+	test_formatLogLevel[debug](){
+		${assertEquals} $(LoggerUtil _formatLogLevel debug) _DEBUG_
+	}
+
 	test_formatLogLevel[error](){
 		${assertEquals} $(LoggerUtil _formatLogLevel error) _ERROR_
 	}
 
 	test_formatLogLevel[info](){
 		${assertEquals} $(LoggerUtil _formatLogLevel info) __INFO_
+	}
+
+	test_formatLogLevel[success](){
+		${assertEquals} $(LoggerUtil _formatLogLevel success) SUCCESS
 	}
 
 	local assertEquals="TestUtil assertEquals"
