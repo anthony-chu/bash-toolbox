e7f486ffb5db4d6139eda5152052faac9cc16575
Anthony Chu <anthony.chu@liferay.com>
2017-12-13 11:42:39 -0800

    Copy getValidFunctions logic into getTestCommands (no logic change)

--- command/validator/CommandValidator.sh
+++ command/validator/CommandValidator.sh
@@ -29,6 +29,30 @@ CommandValidator(){
 		echo ${validFunctions[@]}
 	}
 
+	getTestCommands(){
+		local lineNumber=1
+		local file=${1}
+		local validFunctions=()
+
+		while read line; do
+			if [[ ${line} == *\(\){ ]]; then
+				local prevLineNumber=$((${lineNumber}-1))
+
+				local prevLineContent=$(sed "${prevLineNumber}q;d" ${file})
+
+				if [[ ! ${prevLineContent} =~ @ignore &&
+					! ${prevLineContent} =~ @private ]]; then
+
+					validFunctions+=($(StringUtil strip line \(\)\{))
+				fi
+			fi
+
+			local lineNumber=$((${lineNumber}+1))
+		done < ${file}
+
+		echo ${validFunctions[@]}
+	}
+
 	validateCommand(){
 		local validCommands=($(getValidFunctions ${1}))
 
