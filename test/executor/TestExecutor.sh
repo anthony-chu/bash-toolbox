f27be0e336d2a8a05ff428327c06380199af20fc
Anthony Chu <anthony.chu@liferay.com>
2017-12-12 16:28:45 -0800

    Determine metrics for pass/fail/skipped tests in a functional group

--- test/executor/TestExecutor.sh
+++ test/executor/TestExecutor.sh
@@ -2,10 +2,14 @@ include command.validator.CommandValidator
 
 include logger.Logger
 
+include string.util.StringUtil
 include string.validator.StringValidator
 
 TestExecutor(){
 	executeTest(){
+		local results_debug=()
+		local results_fail=()
+		local results_pass=()
 		local testClass=${1}
 
 		local path=($(echo ${testClass} | sed "s#\([A-Z]\)# \1#g"))
@@ -30,16 +34,35 @@ TestExecutor(){
 
 				if [[ $(StringValidator isSubstring status PASS) ]]; then
 					local logLevel=Success
+
+					results_pass+=(${_test})
 				elif [[ $(StringValidator isSubstring status FAIL) ]]; then
 					local logLevel=Error
+
+					results_fail+=(${_test})
 				else
 					local logLevel=Debug
 					local status=SKIPPED
+
+					results_debug+=(${_test})
 				fi
 
 				Logger log${logLevel}Msg "${testClass}#${_test}_${status}"
 			fi
 		done
+
+		local total=$((
+			${#results_debug[@]}+${#results_fail[@]}+${#results_pass[@]}
+		))
+
+		local message=(
+			${total}_tests_found,
+			${#results_pass[@]}_tests_passed,
+			${#results_fail[@]}_tests_failed,
+			${#results_debug[@]}_tests_skipped
+		)
+
+		Logger logInfoMsg "$(StringUtil join message _)"
 	}
 
 	$@
