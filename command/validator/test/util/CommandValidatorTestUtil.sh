c2ed66de31d543e625617a815e9b392698c912a7
Anthony Chu <anthony.chu@liferay.com>
2017-12-13 16:45:34 -0800

    No need for source as the first line in test file

--- command/validator/test/util/CommandValidatorTestUtil.sh
+++ command/validator/test/util/CommandValidatorTestUtil.sh
@@ -6,7 +6,6 @@ include string.util.StringUtil
 CommandValidatorTestUtil(){
 	writeBashFile(){
 		local content=(
-			"source bash-toolbox/init.sh\n\n"
 			"Test(){\n\t@ignore\n\talpha(){\n\t\techo\n\t}"
 			"\n\n\t@private\n\tbeta(){\n\t\techo\n\t}"
 			"\n\n\t@test\n\tkappa(){\n\t\techo\n\t}\n\n\t\$@\n}"
