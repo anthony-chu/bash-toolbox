d21fe102e92a1188478cd1ecf9d664f8cb27fb66
Anthony Chu <anthony.chu@liferay.com>
2017-12-14 15:58:41 -0800

    Add new function to test file for @description tag

--- command/validator/test/util/CommandValidatorTestUtil.sh
+++ command/validator/test/util/CommandValidatorTestUtil.sh
@@ -7,9 +7,11 @@ include string.util.StringUtil
 CommandValidatorTestUtil(){
 	writeBashFile(){
 		local content=(
-			"@class\nTest(){\n\t@ignore\n\talpha(){\n\t\techo\n\t}"
-			"\n\n\t@private\n\tbeta(){\n\t\techo\n\t}"
-			"\n\n\t@test\n\tkappa(){\n\t\techo\n\t}\n\n\t\$@\n}"
+			"@class\nTest(){\n\t@description\n\talpha(){\n\t\techo\n\t}"
+			"\n\n\t@ignore\n\tbeta(){\n\t\techo\n\t}"
+			"\n\n\t@private\n\tkappa(){\n\t\techo\n\t}"
+			"\n\n\t@test\n\tdelta(){\n\t\techo\n\t}"
+			"\n\n\t\$@\n}"
 		)
 
 		FileWriter append $(FileUtil makeFile ${1}) $(StringUtil join content)
