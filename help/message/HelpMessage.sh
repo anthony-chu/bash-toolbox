850c2a9840b6af7454c765a3ed81cf3731a094dd
Anthony Chu <anthony.chu@liferay.com>
2018-01-30 13:32:37 -0800

    Appending cannot be done on a local variable

--- help/message/HelpMessage.sh
+++ help/message/HelpMessage.sh
@@ -83,7 +83,7 @@ HelpMessage(){
 
 			local paramMap=($(ArrayUtil appendArrayEntry paramMap))
 
-			local message+=("Parameters:")
+			message+=("Parameters:")
 
 			for (( i=0; i<${#paramDescriptionMap[@]}; i++ )); do
 				message+=("\t${paramMap[i]}..........${paramDescriptionMap[i]}")
