1f8ea708a247ea97da7102fcd18e50180f1cc546
Anthony Chu <anthony.chu@liferay.com>
2018-02-22 13:10:19 -0800

    Wrap inside main method

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
-__files=(
-	colorme.sh
-	include.sh
-	markups.sh
-	package.sh
-	readvar.sh
-)
+main(){
+	__files=(
+		colorme.sh
+		include.sh
+		markups.sh
+		package.sh
+		readvar.sh
+	)
 
-for file in ${__files[@]}; do
-	source ${projectDir}bash-toolbox/lib/${file}
-done
+	for file in ${__files[@]}; do
+		source ${projectDir}bash-toolbox/lib/${file}
+	done
+}
+
+main
