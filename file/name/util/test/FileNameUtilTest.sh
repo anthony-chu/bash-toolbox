9f1ba04e089a9f52abebe78e25bb1dc9abb94ba0
Anthony Chu <anthony.chu@liferay.com>
2018-03-15 15:39:32 -0700

    Test using helper methods instead

--- file/name/util/test/FileNameUtilTest.sh
+++ file/name/util/test/FileNameUtilTest.sh
@@ -8,28 +8,28 @@ FileNameUtilTest(){
 	testGetPathConvertToNixPath(){
 		local path="D:/foo/bar"
 
-		${assertEquals} $(FileNameUtil getPath nix ${path}) /d/foo/bar
+		${assertEquals} $(FileNameUtil _getPathUnix ${path}) /d/foo/bar
 	}
 
 	@test
 	testGetPathConvertToWinPath(){
 		local path="/d/foo/bar"
 
-		${assertEquals} $(FileNameUtil getPath win ${path}) D:/foo/bar
+		${assertEquals} $(FileNameUtil _getPathWin ${path}) D:/foo/bar
 	}
 
 	@test
 	testGetPathPreserveNixPath(){
 		local path="/d/foo/bar"
 
-		${assertEquals} ${path} $(FileNameUtil getPath nix ${path})
+		${assertEquals} ${path} $(FileNameUtil _getPathUnix ${path})
 	}
 
 	@test
 	testGetPathPreserveWinPath(){
 		local path="D:/foo/bar"
 
-		${assertEquals} ${path} $(FileNameUtil getPath win ${path})
+		${assertEquals} ${path} $(FileNameUtil _getPathWin ${path})
 	}
 
 	local assertEquals="TestUtil assertEquals"
