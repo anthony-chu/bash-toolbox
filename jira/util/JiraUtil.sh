6eeffd64c66adb7559581fcecde523b0b6b40d82
Anthony Chu <anthony.chu@liferay.com>
2018-03-30 09:13:46 -0700

    Check for redirects

--- jira/util/JiraUtil.sh
+++ jira/util/JiraUtil.sh
@@ -8,7 +8,7 @@ JiraUtil(){
 		local ticket=https://issues.liferay.com/browse/${1}
 
 		case $(CurlUtil getHTTPResponse ${ticket}) in
-			200 ) start ${ticket} ;;
+			200 | 302 ) start ${ticket} ;;
 			* ) Logger logErrorMsg "_${1}_is_not_a_valid_JIRA_issue_key" ;;
 		esac
 	}
