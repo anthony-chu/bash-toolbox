fce03d422677f6867f25189d18fc3cadce26c100
Anthony Chu <anthony.chu@liferay.com>
2018-05-16 11:00:08 -0700

    Add tests

--- matcher/test/MatcherTest.sh
+++ matcher/test/MatcherTest.sh
@@ -5,6 +5,33 @@ include test.util.TestUtil
 @class
 MatcherTest(){
 	@test
+	testReplaceMultipleGroups(){
+		local pattern='\([0-9]\+\)-[a-z]\+-\([0-9]\+\)'
+		local replace='\1+\2'
+		local string="123-abc-123"
+
+		TestUtil assertEquals $(Matcher replace pattern string replace) "123+123"
+	}
+
+	@test
+	testReplaceSingleGroup(){
+		local pattern='[0-9]\+-\([a-z]\+\)-[0-9]\+'
+		local replace='\1'
+		local string="123-abc-123"
+
+		TestUtil assertEquals $(Matcher replace pattern string replace) abc
+	}
+
+	@test
+	testReplacePatternWithString(){
+		local pattern='[0-9]\+-[a-z]\+-[0-9]\+'
+		local replace="xyz"
+		local string="123-abc-123"
+
+		TestUtil assertEquals $(Matcher replace pattern string replace) xyz
+	}
+
+	@test
 	testMatcherMatchesPattern(){
 		local _string=123abc123
 
