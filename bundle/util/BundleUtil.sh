420aa6070e80a9443091f9483817abbce5eed2aa
Anthony Chu <anthony.chu@liferay.com>
2018-06-12 16:32:35 -0700

    Extend timeout for jboss/wildfly

--- bundle/util/BundleUtil.sh
+++ bundle/util/BundleUtil.sh
@@ -36,6 +36,10 @@ BundleUtil(){
 			${replace} ${file} MaxMetaspaceSize=512m MaxMetaspaceSize=1024m
 			${replace} ${file} timeout=480 timeout=6000
 			${replace} ${file} Xmx1024m Xmx2048m
+
+			local s=${appServerDir}/standalone/configuration/standalone.xml
+
+			${replace} ${s} "\(deployment-timeout\)=\"[0-9]+\"" "\1=\"60000\""
 		fi
 
 		Logger logCompletedMsg
