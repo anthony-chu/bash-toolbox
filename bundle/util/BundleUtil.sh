990717fa9f9d83ca0857efbfc21ac8c08719f3cd
Anthony Chu <anthony.chu@liferay.com>
2018-06-15 13:56:47 -0700

    Fix regex spacing

--- bundle/util/BundleUtil.sh
+++ bundle/util/BundleUtil.sh
@@ -20,11 +20,11 @@ BundleUtil(){
 			local tomcatReplace="${replace} ${appServerDir}/bin/setenv.sh"
 
 			if [[ ! $(StringValidator isSubstring branch 6) ]]; then
-				${tomcatReplace}' Xmx[0-9]\+m' Xmx2048m
+				${tomcatReplace} 'Xmx[0-9]\+m' Xmx2048m
 
 				${tomcatReplace} 'XX:MaxPermSize=[0-9]\+m' Xms1024m
 			else
-				${tomcatReplace}' Xmx[0-9]\+m' 'Xmx2048m -Xms1024m'
+				${tomcatReplace} 'Xmx[0-9]\+m' 'Xmx2048m -Xms1024m'
 
 				${tomcatReplace} '\(MaxPermSize\)=[0-9]\+' '\1=512'
 			fi
@@ -35,7 +35,7 @@ BundleUtil(){
 
 			${replace} ${file} '\(MaxMetaspaceSize\)=[0-9]\+m' '\1=1024m'
 			${replace} ${file} '\(timeout\)=[0-9]\+' '\1=6000'
-			${replace} ${file}' Xmx[0-9]\+m' 'Xmx2048m -Xms1024m'
+			${replace} ${file} 'Xmx[0-9]\+m' 'Xmx2048m -Xms1024m'
 
 			local s=${appServerDir}/standalone/configuration/standalone.xml
 
