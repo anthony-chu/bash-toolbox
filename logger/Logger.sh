3a2f11cb8fbb32357b63fc6b7ddf1edee71e467f
Anthony Chu <anthony.chu@liferay.com>
2018-07-18 15:25:05 -0700

    Wrap util function

--- logger/Logger.sh
+++ logger/Logger.sh
@@ -2,6 +2,16 @@ include logger.util.LoggerUtil
 
 @class
 Logger(){
+	log(){
+		@param the_log_level_for_the_log_message
+		local logLevel=${1}
+
+		@param the_log_message
+		local logMessage=${2}
+
+		LoggerUtil getLogMsg ${logLevel} ${logMessage}
+	}
+
 	logCompletedMsg(){
 		logInfoMsg completed
 	}
