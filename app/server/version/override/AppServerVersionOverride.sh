0d71334030ddd6f6899ba497648e86cdaa132964
Anthony Chu <anthony.chu@liferay.com>
2018-09-13 13:17:43 -0700

    Rename variable to be more clear

--- app/server/version/override/AppServerVersionOverride.sh
+++ app/server/version/override/AppServerVersionOverride.sh
@@ -7,15 +7,15 @@ include string.validator.StringValidator
 @class
 AppServerVersionOverride(){
 	getTomcatVersion(){
-		if [[ $(StringValidator isSubstring ${branch} 7.0) ]]; then
+		if [[ $(StringValidator isSubstring ${version} 7.0) ]]; then
 			echo 8.0.53
-		elif [[ $(StringValidator isSubstring ${branch} 6.2) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} 6.2) ]]; then
 			echo 7.0.90
-		elif [[ $(StringValidator isSubstring ${branch} 6.1) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} 6.1) ]]; then
 			echo 7.0.40
-		elif [[ $(StringValidator isSubstring ${branch} 6.0.12) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} 6.0.12) ]]; then
 			echo 6.0.32
-		elif [[ $(StringValidator isSubstring ${branch} 6.0.6) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} 6.0.6) ]]; then
 			echo 6.0.29
 		else
 			AppServerVersionConstants TOMCAT_VERSION
@@ -23,26 +23,26 @@ AppServerVersionOverride(){
 	}
 
 	getWebsphereVersion(){
-		if [[ $(StringValidator isSubstring ${branch} 7.1) || $(
-				StringValidator isSubstring ${branch} master) ]]; then
+		if [[ $(StringValidator isSubstring ${version} 7.1) || $(
+				StringValidator isSubstring ${version} master) ]]; then
 
 			AppServerVersionConstants WEBSPHERE_VERSION
-		elif [[ $(StringValidator isSubstring ${branch} 7.0) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} 7.0) ]]; then
 			echo 8.5.5.0
 		fi
 	}
 
 	getWildflyVersion(){
-		if [[ $(StringValidator isSubstring ${branch} 7.0) ]]; then
+		if [[ $(StringValidator isSubstring ${version} 7.0) ]]; then
 			echo 10.0.0
-		elif [[ $(StringValidator isSubstring ${branch} master) ||
-				$(StringValidator isSubstring ${branch} 7.1) ]]; then
+		elif [[ $(StringValidator isSubstring ${version} master) ||
+				$(StringValidator isSubstring ${version} 7.1) ]]; then
 
 			AppServerVersionConstants WILDFLY_VERSION
 		fi
 	}
 
-	local branch=${2}
+	local version=${2}
 
 	$@
 }
