47bd68662d138ea97dc6f1ec9b7b8aefecbb1a57
Anthony Chu <anthony.chu@liferay.com>
2018-09-19 09:33:13 -0700

    Add tomcat versions for release bundles

--- app/server/version/override/AppServerVersionOverride.sh
+++ app/server/version/override/AppServerVersionOverride.sh
@@ -19,8 +19,14 @@ AppServerVersionOverride(){
 	}
 
 	getTomcatVersion(){
-		if [[ $(StringValidator isSubstring ${version} 7.0) ]]; then
+		if [[ $(StringValidator isSubstring ${version} 7.1.10) ]]; then
+			echo 9.0.6
+		elif [[ $(StringValidator isSubstring ${version} 7.0.10) ]]; then
+			echo 8.0.32
+		elif [[ $(StringValidator isSubstring ${version} 7.0) ]]; then
 			echo 8.0.53
+		elif [[ $(StringValidator isSubstring ${version} 6.2.10) ]]; then
+			echo 7.0.62
 		elif [[ $(StringValidator isSubstring ${version} 6.2) ]]; then
 			echo 7.0.90
 		elif [[ $(StringValidator isSubstring ${version} 6.1) ]]; then
