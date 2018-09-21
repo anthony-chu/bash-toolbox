823662dd6a9418f5317818c0e40b738127e475e9
Anthony Chu <anthony.chu@liferay.com>
2018-09-21 16:48:01 -0700

    Use directly

--- command/exception/CommandException.sh
+++ command/exception/CommandException.sh
@@ -3,11 +3,9 @@ include logger.Logger
 @class
 CommandException(){
 	noSuchCommandException(){
-		${_log} error "\"${1}\"_is_not_a_valid_command"
+		Logger log error "\"${1}\"_is_not_a_valid_command"
 		exit
 	}
-
-	local _log="Logger log"
 
 	$@
 }
