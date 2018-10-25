5051f396d1e3aa09ccd264d77128ed3c72624aff
Anthony Chu <anthony.chu@liferay.com>
2018-10-25 16:11:44 -0700

    Fix database name

--- props/writer/ext/DefaultPortalPropsWriter.sh
+++ props/writer/ext/DefaultPortalPropsWriter.sh
@@ -59,7 +59,7 @@ DefaultPortalPropsWriter(){
     }
 
     writeDatabaseProps(){
-        local databaseName=lportal${branch}
+        local databaseName=lportal${branch//-/}
 
         local propsKeyValueList=(
             jdbc.default.jndi.name=
